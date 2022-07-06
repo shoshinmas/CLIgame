b386ad47f68f81a15f813b1a9758f485f90059b8
shoshinmas <shoshinmas@protonmail.com>
2022-07-06 16:46:49 +0200

    App not working if not level 1-5

--- lib/App.php
+++ lib/App.php
@@ -35,45 +35,43 @@ class App
         if (isset($argv[1])) {
             $name = $argv[1];
         }
-        $prize = new Prize(0,0);
+        $prize = new Prize(0, 0);
         $this->getPrinter()->display("Hello $name! Welcome to magnificient world of gambling. If you feel lucky today, 
         choose your level of difficulty (between 1 and 5, where 1 is the highest");
         $levelInput = (int)$this->getPrinter()->takeinput();
-        $level = new GameConditions($levelInput, $levelInput);
-        $level->setCalcTries($levelInput);
-        $level->setDrawTries($levelInput);
-        $this->getPrinter()->display("Well done, $name! We will now display an equation. You simply have to type in your 
+        if (($levelInput >= 1) && ($levelInput <= 5)) {
+            $level = new GameConditions($levelInput, $levelInput);
+            $level->setCalcTries($levelInput);
+            $level->setDrawTries($levelInput);
+            $this->getPrinter()->display("Well done, $name! We will now display an equation. You simply have to type in your 
         result");
-        $x = $this->getDrawResult()->generateDraw();
-        $y = $this->getDrawResult()->generateDraw();
-        $calc = new Calculator($x, $y);
-        $calcResult = $calc->add();
-        $this->getPrinter()->display("$x + $y = ");
-        $calcInput = (int) $this->getPrinter()->takeinput();
-        $winOne = new WinChecker($calcInput, $calcResult, $level->getCalcTries());
-        if($winOne->checkCalc())
-        {
-            $prize->setCalcInput($calcInput);
-            $this->getPrinter()->display("Well done, $name! We will now draw a number between 1 and 100. You simply have 
+            $x = $this->getDrawResult()->generateDraw();
+            $y = $this->getDrawResult()->generateDraw();
+            $calc = new Calculator($x, $y);
+            $calcResult = $calc->add();
+            $this->getPrinter()->display("$x + $y = ");
+            $calcInput = (int)$this->getPrinter()->takeinput();
+            $winOne = new WinChecker($calcInput, $calcResult, $level->getCalcTries());
+            if ($winOne->checkCalc()) {
+                $prize->setCalcInput($calcInput);
+                $this->getPrinter()->display("Well done, $name! We will now draw a number between 1 and 100. You simply have 
             to guess your result. Type it below");
-            $z = $this->getDrawResult()->generateDraw();
-            $drawInput = (int) $this->getPrinter()->takeinput();
-            $winTwo = new WinChecker($drawInput, $z, $level->getDrawTries());
-            if ($winTwo->checkDraw())
-            {
-                $prize->setDrawInput($drawInput);
-                echo "Congratulations, you won a prize\n";
-                $prize->drawPrize();
-            }
-            else
-            {
+                $z = $this->getDrawResult()->generateDraw();
+                $drawInput = (int)$this->getPrinter()->takeinput();
+                $winTwo = new WinChecker($drawInput, $z, $level->getDrawTries());
+                if ($winTwo->checkDraw()) {
+                    $prize->setDrawInput($drawInput);
+                    echo "Congratulations, you won a prize\n";
+                    $prize->drawPrize();
+                } else {
+                    echo "Game over";
+                    exit();
+                }
+            } else {
                 echo "Game over";
                 exit();
             }
         }
-        else {
-            echo "Game over";
-            exit();
-        }
+        else echo "Wrong level";
     }
 }
