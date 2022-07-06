2735eca477a1176b698a841a9b0fbef39b66b03b
shoshinmas <shoshinmas@protonmail.com>
2022-07-06 16:53:05 +0200

    Code cleaned

--- lib/Draw.php
+++ lib/Draw.php
@@ -6,25 +6,9 @@ namespace CLIgame;
 
 class Draw
 {
-    private int $drawResult;
+
     public function __construct()
     {
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getDrawResult()
-    {
-        return $this->drawResult;
-    }
-
-    /**
-     * @param mixed $drawResult
-     */
-    public function setDrawResult(int $drawResult): void
-    {
-        $this->drawResult = $drawResult;
     }
 
     public function generateDraw(): int
